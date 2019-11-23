dd0f36e239c8be193fc8f162cb70422d3b8b0a40
Aileen Murphy <ai.murf2@gmail.com>
2019-11-22 23:07:59 -0800

    starting the app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="hero">
+      <section className="header">
+      <h1>Boats 'n Moor!</h1>
+      <p>Helping find moor(age) for boats and boats to moor.</p>
+    </section>
     </div>
+    </>
   );
 }
 
